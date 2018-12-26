e015186e8259eb6434810641e7b90e869c855117
ziyuanzhang <2661548928@qq.com>
2018-12-26 22:39:08 +0800

    安装betterscroll

---
+++ src/components/betterScroll.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>better-scroll</h1>
+        <div class="scrollLeft">
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+            <div>hahahha</div>
+        </div>
+        <div class="scrollRight"></div>
+    </div>
+</template>
+<script>
+import Bscroll from 'better-scroll'
+export default {
+    
+}
+</script>
+<style>
+
+</style>
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import home from "@/components/home";
 import axiosRequest from "@/components/axiosRequest";
+import betterScroll from "@/components/betterScroll";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/axiosRequest",
       name: "axiosRequest",
       component: axiosRequest
+    },
+    {
+      path: "/betterScroll",
+      name: "betterScroll",
+      component: betterScroll
     }
   ]
 });
